4bf2ab6875db15d7cffe374fc95430c9d502fec2
EnzoVianaDeAndrade1819 <96305131+EnzoVianaDeAndrade1819@users.noreply.github.com>
2022-09-01 11:00:16 -0300

    Delete assets/preload/stages directory

--- assets/preload/stages/Dside.lua
+++
@@ -1,9 +0,0 @@
-function onCreate()
-	-- background shit
-
-	makeLuaSprite('Dside', 'Dside', -600, 10);
-	setLuaSpriteScrollFactor('Dside', 0.8, 0.8);
-	scaleObject('Dside', 0.8, 0.8);
-                addLuaSprite('Dside',false)
-
-end
--- assets/preload/stages/alien.lua
+++
@@ -1,9 +0,0 @@
-function onCreate()
-	-- background shit
-
-	makeLuaSprite('alien', 'alien', -300, 30);
-	setLuaSpriteScrollFactor('alien', 0.8, 0.8);
-	scaleObject('alien', 0.8, 0.8);
-                addLuaSprite('alien',false)
-
-end
