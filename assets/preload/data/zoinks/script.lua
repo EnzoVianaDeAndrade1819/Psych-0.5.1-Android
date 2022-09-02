573772ef529f278bdd269d0a5ab3fbfd5d3898af
EnzoVianaDeAndrade1819 <96305131+EnzoVianaDeAndrade1819@users.noreply.github.com>
2022-09-02 09:44:55 -0300

    Delete assets/preload/data/zoinks directory

--- assets/preload/data/zoinks/script.lua
+++
@@ -1,8 +0,0 @@
-function onCreate()
-	setPropertyFromClass('GameOverSubstate', 'characterName', 'replay'); --Character json file for the death animation
-	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'ded'); --put in mods/sounds/
-	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'wtf'); --put in mods/music/
-	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'end'); --put in mods/music/
-end
-
-
